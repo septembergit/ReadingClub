7318f0bdb2007740b8a23309e516c70439994ceb
shiyusarah <shiyusarah@didichuxing.com>
2017-04-06 10:02:47 +0800

    04-06 清空浏览器缓存代码运行

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -8,7 +8,7 @@ mongoose.connection.on('connected', function () {
     console.log('Mongoose connected to ' + dbURI);
 });
 mongoose.connection.on('error', function (err) {
-    console.log('Mongoose connection errorsssssssssss: ' + err);
+    console.log('Mongoose connection error: ' + err);
 });
 mongoose.connection.on('disconnected', function () {
     console.log('Mongoose disconnected');
