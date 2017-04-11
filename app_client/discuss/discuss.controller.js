be480088af32c397be58e2992d6d4af719db5481
shiyusarah <shiyusarah@didichuxing.com>
2017-04-11 17:10:47 +0800

    04-11

--- app_client/discuss/discuss.controller.js
+++ app_client/discuss/discuss.controller.js
@@ -5,14 +5,13 @@ discussCtrl.$inject = ['talksData', '$location', 'authentication'];
 
 function discussCtrl(talksData, $location, authentication) {
     var vm = this;
-    vm.talksList = talksData.getTalks();        // 测试假数据
-    // vm.message = "loading...";
-    // booksData.getBooks.success(function (data) {
-    //     vm.message = data.length > 0 ? "" : "暂无数据";
-    //     vm.books = data;
-    // }).error(function () {
-    //     vm.message = "Sorry, something's gone wrong ";
-    // });
+    vm.message = "loading...";
+    talksData.getTalks.success(function (data) {
+        vm.message = data.length > 0 ? "" : "暂无数据";
+        vm.talksList = data;
+    }).error(function () {
+        vm.message = "Sorry, something's gone wrong ";
+    });
     vm.user = authentication.currentUser();
     vm.isLoggedIn = authentication.isLoggedIn();
     vm.currentPath = $location.path();
--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -5,12 +5,11 @@ homeCtrl.$inject = ['topicData'];
 
 function homeCtrl(topicData) {
     var vm = this;
-    vm.data = topicData;       // 测试假数据
-    // vm.message = "loading...";
-    // topicData.success(function (data) {
-    //     vm.message = data.length > 0 ? "" : "暂无数据";
-    //     vm.data = data;
-    // }).error(function () {
-    //     vm.message = "Sorry, something's gone wrong ";
-    // });
+    vm.message = "loading...";
+    topicData.success(function (data) {
+        vm.message = data.length > 0 ? "" : "暂无数据";
+        vm.data = data;
+    }).error(function () {
+        vm.message = "Sorry, something's gone wrong ";
+    });
 }
