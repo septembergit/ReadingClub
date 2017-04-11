6a4f9fe1970a96a955f37f7e763202cd27d8941b
shiyusarah <shiyusarah@didichuxing.com>
2017-04-11 15:48:13 +0800

    04-11

--- app_client/main/directive/header/header.controller.js
+++ app_client/main/directive/header/header.controller.js
@@ -5,13 +5,9 @@ headerCtrl.$inject = ['$location', 'authentication'];
 
 function headerCtrl($location, authentication) {
     var vm = this;
-    vm.currentUser = {     // 测试假数据
-        email: 'test@163.com',
-        name: 'test'
-    };
     vm.currentPath = $location.path();
-    // vm.isLoggedIn = authentication.isLoggedIn();
-    // vm.currentUser = authentication.currentUser();
+    vm.isLoggedIn = authentication.isLoggedIn();
+    vm.currentUser = authentication.currentUser();
     vm.logout = function () {
         authentication.logout();
         $location.path('/');
